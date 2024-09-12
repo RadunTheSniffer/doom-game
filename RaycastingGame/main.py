ea4b861fe46db3a1995b57fca8dea9a9671a2b34
RadunTheSniffer <ridhuan.work2001@gmail.com>
2024-09-12 15:43:49 +0800

    latest update

--- RaycastingGame/main.py
+++ RaycastingGame/main.py
@@ -17,6 +17,7 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
 
     def update(self):
@@ -28,6 +29,7 @@ class Game:
 
     def draw(self):
         self.screen.fill('black')
+        self.object_renderer.draw()
         #self.map.draw()
         #self.player.draw()
     
